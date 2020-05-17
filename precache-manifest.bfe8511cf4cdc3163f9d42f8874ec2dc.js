fee7829754657cc052b8b4a622e61117de9ef8f4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-17 13:39:24 +0000

    Deploy pedroblanc/pedroblanc.github.io to github.com/pedroblanc/pedroblanc.github.io.git:master

---
+++ precache-manifest.bfe8511cf4cdc3163f9d42f8874ec2dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20f8f80bba75b38f029e1b33c4de867",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16e31292182312481dda",
+    "url": "/static/css/main.d50bb397.chunk.css"
+  },
+  {
+    "revision": "a56f3c05802e03f7a854",
+    "url": "/static/js/2.1a4efe0c.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.1a4efe0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e31292182312481dda",
+    "url": "/static/js/main.7a0b0b65.chunk.js"
+  },
+  {
+    "revision": "9f5778c51c2f6184d777",
+    "url": "/static/js/runtime-main.8567c174.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/p5x-playground/precache-manifest.b122f64669012c552f65593b7263af57.js"
+  "/precache-manifest.bfe8511cf4cdc3163f9d42f8874ec2dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/p5x-playground/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
